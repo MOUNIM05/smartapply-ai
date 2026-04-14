1ce9c3a681e33469776014166bac0fae2abc5241
asfoutymouhcine-ai <asfoutymouhcine@gmail.com>
2026-04-14 16:13:05 +0100

    feat: improve account settings and remember me login

--- Backend/auth_service/app/main.js
+++ Backend/auth_service/app/main.js
@@ -13,7 +13,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(morgan("dev"));
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 app.get("/", (req, res) => {
   res.status(200).json({
